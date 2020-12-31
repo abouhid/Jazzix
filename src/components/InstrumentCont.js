3b4c8a7dfdd565cfba52196bf137309f89b58016
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-12-31 14:18:00 -0300

    finish DB

--- src/components/InstrumentCont.js
+++ src/components/InstrumentCont.js
@@ -1,138 +1,50 @@
 import React, { useContext } from 'react';
 import Drums from './Instruments/Drums';
 import { Context } from '../Context';
+import Guitars from './Instruments/Guitars';
+import Pianos from './Instruments/Pianos';
+import Trumpets from './Instruments/Trumpets';
 
 function InstrumentCont() {
   const { storedata } = useContext(Context);
-  return (
-    <>
+
+  const renderComp = id => {
+    switch (id) {
+      case 0:
+        return <Drums id={id} />;
+      case 1:
+        return <Guitars id={id} />;
+      case 2:
+        return <Pianos id={id} />;
+      case 3:
+        return <Trumpets id={id} />;
+      default:
+        return console.log(`Sorry, we are out of ${id}.`);
+    }
+  };
+
+  const lists = storedata.map(el => (
+    <React.Fragment key={el.id}>
       <div className="d-flex align-items-baseline m-0">
         <h4 className="mt4 dark font-weight-bold fs3 title">
           <i className="fas fa-drum" />
-          {storedata[0].name}
+          {storedata[el.id].name}
         </h4>
         <p className="mt4 ml-4 grey font-weight-bold m-0 fs2">
-          {storedata[0].description}
+          {storedata[el.id].description}
         </p>
       </div>
       <hr className="bg-grey mt-0" />
       <ul className="products">
-        <Drums />
+        {renderComp(el.id)}
       </ul>
 
-    </>
+    </React.Fragment>
+
+  ));
+  return (
+    [lists]
   );
 }
 
 export default InstrumentCont;
-
-/* eslint-disable */
-{/*<div class="d-flex align-items-baseline m-0">
-        <h4 class="mt4 dark font-weight-bold fs3 title"><i class="fas fa-drum"></i> Drum kits</h4>
-        <p class="mt4 ml-4 grey font-weight-bold m-0 fs2">(Drums, cymbals, racks and much more)</p>
-      </div>
-      <hr class="bg-grey mt-0">
-      <ul class="products">
-        <li>
-          <img
-            src="https://gloimg.gbtcdn.com/images/pdm-product-pic/Electronic/2019/05/10/source-img/20190510143637_37273.jpg_100x100.jpg"
-            alt="product"/>
-          <div>
-            <h4 class="font-weight-bold fs2"><span>SLADE 14 inch Snare Drum with Stick Strap Wrench - Red Wine 4</span></h4>
-            <h5 class="red fs2">New</h5>
-            <h5 class="fs2"><strong>From:</strong> Juiz de Fora, Brazil</h5>
-
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="dropdown size m-0">
-                <button class="btn btn-light p-0" type="button" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false">
-                  <i class="fas fa-plus-square red fs3 font-weight-bold"></i>
-                </button>
-                <div class="dropdown-menu border">
-                  <span class="dropdown-item" >Add to Wishlist</span>
-                  <span class="dropdown-item" >Send a Message</span>
-                  <span class="dropdown-item" >More Details</span>
-                </div>
-              </div>
-              <h4 class="red font-weight-bold fs3 m-0"> <span class="red" >$57.93</span></h4>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://media.musiciansfriend.com/is/image/MMGS7/Effects-Cymbal-Stack/K36911000000000-00-220x220.jpg"
-            alt="product">
-          <div>
-            <h4 class="font-weight-bold fs2"><span>Agazarian Effects Cymbal Stack</span></h4>
-            <h5 class="red fs2">Used</h5>
-            <h5 class="fs2"><strong>From:</strong> Além Paraíba, Brazil</h5>
-
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="dropdown size m-0">
-                <button class="btn btn-light p-0" type="button" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false">
-                  <i class="fas fa-plus-square red fs3 font-weight-bold"></i>
-                </button>
-                <div class="dropdown-menu border">
-                  <span class="dropdown-item" >Add to Wishlist</span>
-                  <span class="dropdown-item" >Send a Message</span>
-                  <span class="dropdown-item" >More Details</span>
-                </div>
-              </div>
-              <h4 class="red font-weight-bold fs3 m-0"> <span class="red" >$37.99</span></h4>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://media.musiciansfriend.com/is/image/MMGS7/5-Piece-Complete-Drum-Set-Black/J56102000002000-00-220x220.jpg"
-            alt="product">
-          <div>
-            <h4 class="font-weight-bold fs2"><span>Rogue 5-Piece Complete Drum Set</span></h4>
-            <h5 class="red fs2">New</h5>
-            <h5 class="fs2"><strong>From:</strong> Rio de Janeiro, Brazil</h5>
-
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="dropdown size m-0">
-                <button class="btn btn-light p-0" type="button" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false">
-                  <i class="fas fa-plus-square red fs3 font-weight-bold"></i>
-                </button>
-                <div class="dropdown-menu border">
-                  <span class="dropdown-item" >Add to Wishlist</span>
-                  <span class="dropdown-item" >Send a Message</span>
-                  <span class="dropdown-item" >More Details</span>
-                </div>
-              </div>
-
-              <h4 class="red font-weight-bold fs3 m-0"> <span class="red" >$349.99</span></h4>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://media.musiciansfriend.com/is/image/MMGS7/SD350-Electronic-Drum-Kit-with-Mesh-Pads/J52903000000000-00-220x220.jpg"
-            alt="product">
-          <div>
-            <h4 class="font-weight-bold fs2"><span>Simmons SD350 Electronic Drum Kit with Mesh Pads</span></h4>
-            <h5 class="red fs2">New</h5>
-            <h5 class="fs2"><strong>From:</strong> Juiz de Fora, Brazil</h5>
-
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="dropdown size m-0">
-                <button class="btn btn-light p-0" type="button" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false">
-                  <i class="fas fa-plus-square red fs3 font-weight-bold"></i>
-                </button>
-                <div class="dropdown-menu border">
-                  <span class="dropdown-item" >Add to Wishlist</span>
-                  <span class="dropdown-item" >Send a Message</span>
-                  <span class="dropdown-item" >More Details</span>
-                </div>
-              </div>
-              <h4 class="red font-weight-bold fs3 m-0"> <span class="red" >$319.99</span></h4>
-            </div>
-          </div>
-        </li>
-      </ul>
-*/}
