4d1d84e402cbc4361bc0a3171b027ed4d5ea3738
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-12-31 13:09:47 -0300

    finish drums containers

--- src/components/InstrumentCont.js
+++ src/components/InstrumentCont.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import Drums from './Instruments/Drums';
 import { Context } from '../Context';
 
 function InstrumentCont() {
@@ -16,154 +17,10 @@ function InstrumentCont() {
       </div>
       <hr className="bg-grey mt-0" />
       <ul className="products">
-        <li>
-          <img
-            src={storedata[0].instrument.img}
-            alt="product"
-          />
-          <div>
-            <h4 className="font-weight-bold fs2"><span>{storedata[0].instrument.name}</span></h4>
-            <h5 className="red fs2">{storedata[0].instrument.state}</h5>
-            <h5 className="fs2">
-              <strong>From:</strong>
-              {storedata[0].instrument.from}
-            </h5>
-
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="dropdown size m-0">
-                <button
-                  className="btn btn-light p-0"
-                  type="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i className="fas fa-plus-square red fs3 font-weight-bold" />
-                </button>
-                <div className="dropdown-menu border">
-                  <span className="dropdown-item">Add to Wishlist</span>
-                  <span className="dropdown-item">Send a Message</span>
-                  <span className="dropdown-item">More Details</span>
-                </div>
-              </div>
-              <h4 className="red font-weight-bold fs3 m-0">
-                <span className="red">
-                  $
-                  {storedata[0].instrument.price}
-                </span>
-              </h4>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://media.musiciansfriend.com/is/image/MMGS7/Effects-Cymbal-Stack/K36911000000000-00-220x220.jpg"
-            alt="product"
-          />
-          <div>
-            <h4 className="font-weight-bold fs2"><span>Agazarian Effects Cymbal Stack</span></h4>
-            <h5 className="red fs2">Used</h5>
-            <h5 className="fs2">
-              <strong>From:</strong>
-              Além Paraíba, Brazil
-            </h5>
-
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="dropdown size m-0">
-                <button
-                  className="btn btn-light p-0"
-                  type="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i className="fas fa-plus-square red fs3 font-weight-bold" />
-                </button>
-                <div className="dropdown-menu border">
-                  <span className="dropdown-item">Add to Wishlist</span>
-                  <span className="dropdown-item">Send a Message</span>
-                  <span className="dropdown-item">More Details</span>
-                </div>
-              </div>
-              <h4 className="red font-weight-bold fs3 m-0">
-                <span className="red">$37.99</span>
-              </h4>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://media.musiciansfriend.com/is/image/MMGS7/5-Piece-Complete-Drum-Set-Black/J56102000002000-00-220x220.jpg"
-            alt="product"
-          />
-          <div>
-            <h4 className="font-weight-bold fs2"><span>Rogue 5-Piece Complete Drum Set</span></h4>
-            <h5 className="red fs2">New</h5>
-            <h5 className="fs2">
-              <strong>From:</strong>
-              Rio de Janeiro, Brazil
-            </h5>
-
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="dropdown size m-0">
-                <button
-                  className="btn btn-light p-0"
-                  type="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i className="fas fa-plus-square red fs3 font-weight-bold" />
-                </button>
-                <div className="dropdown-menu border">
-                  <span className="dropdown-item">Add to Wishlist</span>
-                  <span className="dropdown-item">Send a Message</span>
-                  <span className="dropdown-item">More Details</span>
-                </div>
-              </div>
-
-              <h4 className="red font-weight-bold fs3 m-0">
-                <span className="red">$349.99</span>
-              </h4>
-            </div>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://media.musiciansfriend.com/is/image/MMGS7/SD350-Electronic-Drum-Kit-with-Mesh-Pads/J52903000000000-00-220x220.jpg"
-            alt="product"
-          />
-          <div>
-            <h4 className="font-weight-bold fs2"><span>Simmons SD350 Electronic Drum Kit with Mesh Pads</span></h4>
-            <h5 className="red fs2">New</h5>
-            <h5 className="fs2">
-              <strong>From:</strong>
-              Juiz de Fora, Brazil
-            </h5>
-
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="dropdown size m-0">
-                <button
-                  className="btn btn-light p-0"
-                  type="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i className="fas fa-plus-square red fs3 font-weight-bold" />
-                </button>
-                <div className="dropdown-menu border">
-                  <span className="dropdown-item">Add to Wishlist</span>
-                  <span className="dropdown-item">Send a Message</span>
-                  <span className="dropdown-item">More Details</span>
-                </div>
-              </div>
-              <h4 className="red font-weight-bold fs3 m-0">
-                <span className="red">$319.99</span>
-              </h4>
-            </div>
-          </div>
-        </li>
+        <Drums />
+        <Drums />
+        <Drums />
+        <Drums />
       </ul>
 
     </>
--- src/data/storedata.js
+++ src/data/storedata.js
@@ -1,15 +1,54 @@
 export default [
   {
     id: 0,
-    name: 'Drum Kits',
     description: '(Drums , cymbals, racks and much more)',
-    instrument: {
-      id: 0,
-      img: 'https://gloimg.gbtcdn.com/images/pdm-product-pic/Electronic/2019/05/10/source-img/20190510143637_37273.jpg_100x100.jpg',
-      name: 'SLADE 14 inch Snare Drum with Stick Strap Wrench - Red Wine 4',
-      state: 'New',
-      from: 'Juiz de Fora, Brazil',
-      price: 5.99,
-    },
+    instruments:
+      {
+        id: 0,
+        img: 'https://gloimg.gbtcdn.com/images/pdm-product-pic/Electronic/2019/05/10/source-img/20190510143637_37273.jpg_100x100.jpg',
+        name: 'SLADE 14 inch Snare Drum with Stick Strap Wrench - Red Wine 4',
+        state: 'New',
+        from: 'Juiz de Fora, Brazil',
+        price: 5.99,
+      },
+  },
+  {
+    id: 1,
+    description: '(Drums , cymbals, racks and much more)',
+    instruments:
+      {
+        id: 0,
+        img: 'https://media.musiciansfriend.com/is/image/MMGS7/Effects-Cymbal-Stack/K36911000000000-00-220x220.jpg',
+        name: 'Agazarian Effects Cymbal Stack',
+        state: 'Used',
+        from: 'Além Paraíba, Brazil',
+        price: 37.99,
+      },
+  },
+  {
+    id: 2,
+    description: '(Drums , cymbals, racks and much more)',
+    instruments:
+      {
+        id: 0,
+        img: 'https://media.musiciansfriend.com/is/image/MMGS7/5-Piece-Complete-Drum-Set-Black/J56102000002000-00-220x220.jpg',
+        name: 'Rogue 5-Piece Complete Drum Set',
+        state: 'New',
+        from: 'Rio de Janeiro, Brazil',
+        price: 149.99,
+      },
+  },
+  {
+    id: 3,
+    description: '(Drums , cymbals, racks and much more)',
+    instruments:
+      {
+        id: 0,
+        img: 'https://media.musiciansfriend.com/is/image/MMGS7/SD350-Electronic-Drum-Kit-with-Mesh-Pads/J52903000000000-00-220x220.jpg',
+        name: 'Simmons SD350 Electronic Drum Kit with Mesh Pads',
+        state: 'New',
+        from: 'Juiz de Fora, Brazil',
+        price: 319.99,
+      },
   },
 ];
