711ad88fcf9480fc6c0b459a3aa84d284e36211e
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-01-01 18:07:56 -0300

    added dropdown button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Search() {
+  const [categories, setCategories] = useState('Categories');
+  const [location, setLocation] = useState('Location');
+
   return (
     <>
       <section className="text-center p5 text-light bg-red header">
@@ -9,42 +14,21 @@ function Search() {
         <h4 className="fs3">Buying your instruments the way it should be done</h4>
 
         <ul className="d-flex justify-content-center mt-4">
-          <li className="dropdown size">
-            <button
-              className="btn bg-grey dropdown-toggle"
-              type="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Categories
-            </button>
-            <div className="dropdown-menu">
-              <span className="dropdown-item">Guitar / Bass</span>
-              <span className="dropdown-item">Drums</span>
-              <span className="dropdown-item">Piano / Keyboards</span>
-              <span className="dropdown-item">Others</span>
+          <DropdownButton onClick={e => setCategories(e.target.textContent)} id="dropdown-basic-button" title={categories}>
+            <Dropdown.Item> Guitar </Dropdown.Item>
+            <Dropdown.Item as="button"><div>Drums</div></Dropdown.Item>
+            <Dropdown.Item as="button"><div>Piano / Keyboards</div></Dropdown.Item>
+            <Dropdown.Item as="button"><div>Others</div></Dropdown.Item>
 
-            </div>
-          </li>
-          <li className="dropdown size ml-1">
-            <button
-              className="btn bg-grey dropdown-toggle"
-              type="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Location
-            </button>
-            <div className="dropdown-menu">
-              <span className="dropdown-item">Brazil</span>
-              <span className="dropdown-item">UK</span>
-              <span className="dropdown-item">Poland</span>
-              <span className="dropdown-item">Others</span>
+          </DropdownButton>
 
-            </div>
-          </li>
+          <DropdownButton onClick={e => setLocation(e.target.textContent)} id="dropdown-basic-button" title={location}>
+            <Dropdown.Item> Brazil </Dropdown.Item>
+            <Dropdown.Item as="button"><div>Poland</div></Dropdown.Item>
+            <Dropdown.Item as="button"><div>UK</div></Dropdown.Item>
+            <Dropdown.Item as="button"><div>Others</div></Dropdown.Item>
+
+          </DropdownButton>
           <li className="form-inline size">
             <input className="form-control ml-2 " type="search" placeholder="Start rocking!" aria-label="Search" />
 
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,48 +1,34 @@
 import { Link } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import React, { useState } from 'react';
 import MainSearch from '../components/MainSearch';
 
 function SearchPage() {
+  const [categories, setCategories] = useState('Categories');
+  const [location, setLocation] = useState('Location');
+
   return (
     <>
       <section className=" p5 text-light bg-red header">
 
         <ul className="d-flex m-1">
-          <li className="dropdown size">
-            <button
-              className="btn bg-grey dropdown-toggle"
-              type="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              All Departments
-            </button>
-            <div className="dropdown-menu">
-              <span className="dropdown-item">Guitar / Bass</span>
-              <span className="dropdown-item">Drums</span>
-              <span className="dropdown-item">Piano / Keyboards</span>
-              <span className="dropdown-item">Others</span>
+          <DropdownButton onClick={e => setCategories(e.target.textContent)} id="dropdown-basic-button" title={categories}>
+            <Dropdown.Item> Guitar </Dropdown.Item>
+            <Dropdown.Item as="button"><div>Drums</div></Dropdown.Item>
+            <Dropdown.Item as="button"><div>Piano / Keyboards</div></Dropdown.Item>
+            <Dropdown.Item as="button"><div>Others</div></Dropdown.Item>
 
-            </div>
-          </li>
-          <li className="dropdown size ml-1">
-            <button
-              className="btn bg-grey dropdown-toggle"
-              type="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Location
-            </button>
-            <div className="dropdown-menu">
-              <span className="dropdown-item">Brazil</span>
-              <span className="dropdown-item">UK</span>
-              <span className="dropdown-item">Poland</span>
-              <span className="dropdown-item">Others</span>
+          </DropdownButton>
 
-            </div>
-          </li>
+          <DropdownButton onClick={e => setLocation(e.target.textContent)} id="dropdown-basic-button" title={location}>
+            <Dropdown.Item> Brazil </Dropdown.Item>
+            <Dropdown.Item as="button"><div>Poland</div></Dropdown.Item>
+            <Dropdown.Item as="button"><div>UK</div></Dropdown.Item>
+            <Dropdown.Item as="button"><div>Others</div></Dropdown.Item>
+
+          </DropdownButton>
+
           <li className="form-inline size">
             <input className="form-control ml-2 " type="search" placeholder="Start rocking!" aria-label="Search" />
 
