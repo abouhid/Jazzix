8f66d266f97489904685506fa5da677eb848c3fa
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-01-05 18:26:07 -0300

    fixed iteration issue

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         <Route path="/search">
           <SearchPage />
         </Route>
-        <Route path="/:productType/:productId">
+        <Route path="/info/:productId">
           <InfoPage />
         </Route>
       </Switch>
--- src/pages/InfoPage.js
+++ src/pages/InfoPage.js
@@ -1,57 +1,55 @@
+/*eslint-disable */
 import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { LoremIpsum } from 'react-lorem-ipsum';
 import { Context } from '../Context';
 
 function InfoPage() {
   const { allInstruments, addToCart, addToWishlist } = useContext(Context);
-
-  const { productId, productType } = useParams();
-  const typeObj = allInstruments.find(product => product.id == productType);
+  const { productId } = useParams();
+  const typeObj = allInstruments.find(product => product.instruments.find(inst=>inst.id==productId));
   const instObj = typeObj.instruments.find(inst => inst.id == productId);
   console.log(instObj);
   return (
-    <>
-      <li className="cart-item">
+    <div className="info-item">
+      <img
+        src={instObj.img}
+        alt="img"
+        style={{ width: '100px', height: '130px', margin: '10px 15px 10px 0px' }}
+      />
 
-        <img
-          src={instObj.img}
-          alt="img"
-          style={{ width: '100px', height: '130px', margin: '10px 15px 10px 0px' }}
-        />
+      <div className="flex-column w-100">
+        <h4 className="mb-3">
+          {instObj.name}
+          {' '}
+        </h4>
+        <LoremIpsum p={1} avgSentencesPerParagraph={4} />
 
-        <div className="flex-column w-100">
-          <h4>
-            {instObj.name}
-
-          </h4>
-
-          <h5 className="red fs2">
-            {instObj.state}
-          </h5>
-          <h5 className="fs2">
-
-            <strong>
-              From:
-            </strong>
-            {instObj.from}
-          </h5>
-          <div className="dropdown size m-0">
-
-            <button className="btn   p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i className="fas fa-plus red fs3 font-weight-bold" />
-            </button>
-            <div className="dropdown-menu border">
-              <span aria-hidden="true" onClick={() => addToWishlist(instObj)} className="dropdown-item">Add to Wishlist</span>
-              <span aria-hidden="true" onClick={() => addToCart(instObj)} className="dropdown-item">Add to Cart</span>
-            </div>
+        <h5 className="red fs2">
+          {instObj.state}
+        </h5>
+        <h5 className="fs2">
+          {' '}
+          <strong> From: </strong>
+          {' '}
+          {instObj.from}
+          {' '}
+        </h5>
+        <div className="dropdown size m-0">
+          <button className="btn   p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i className="fas fa-plus red fs3 font-weight-bold" />
+          </button>
+          <div className="dropdown-menu border">
+            <span aria-hidden="true" onClick={() => addToWishlist(instObj)} className="dropdown-item">Add to Wishlist</span>
+            <span aria-hidden="true" onClick={() => addToCart(instObj)} className="dropdown-item">Add to Cart</span>
           </div>
-          <p className="item-price fs3 red">
-            {instObj.price}
-          </p>
-          <hr />
         </div>
-      </li>
-    </>
+        <p className="item-price fs3 red">
+          {instObj.price}
+        </p>
+        <hr />
+      </div>
+    </div>
   );
 }
 
